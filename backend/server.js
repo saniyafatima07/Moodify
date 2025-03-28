b16912f0168812ebe579cd0643f9ff908d2e7e76
saniyafatima07 <saniyafatima200631@gmail.com>
2025-03-28 15:45:24 +0530

    Add .gitignore and create backend folder with necessary files

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+const PORT= process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.send("Backend is running");
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
