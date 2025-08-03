26c850b4bc1272755efa2022abbf50e1834da8ce
Padmashree06 <padmashreess2006@gmail.com>
2025-08-03 17:17:06 +0530

    Modified port

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 const jokes = require("./jokes.json");
 const riddles = require("./riddles.json");
